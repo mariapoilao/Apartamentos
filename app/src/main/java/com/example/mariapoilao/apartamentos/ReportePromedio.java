e65a3dad271dc32167937aabe9ff347019f8af50
Maria <mariapoilao@Marias-MacBook-Pro.local>
2017-05-21 12:30:49 -0500

    Aplicación Terminada

--- app/src/main/java/com/example/mariapoilao/apartamentos/ReportePromedio.java
+++ app/src/main/java/com/example/mariapoilao/apartamentos/ReportePromedio.java
@@ -33,23 +33,55 @@ public class ReportePromedio extends AppCompatActivity {
 
         apartamentos=Datos.traerApartamentos(getApplicationContext());
 
-        int promedio=0, cont=0, aux, tam, suma =0;
+        int promedio1=0, promedio2=0, promedio3=0, promedio4=0, promedio5=0, cont=0, aux, tam, suma1 =0, suma2=0, suma3=0, suma4=0, suma5=0;
         aux=apartamentos.size();
 
         for (int i = 0; i < aux; i++) {
             tam=Integer.parseInt(apartamentos.get(i).getTamaño());
 
 
-            if (apartamentos.get(i).getPiso().equals(this.getResources().getString(R.string.piso4))){
+            if (apartamentos.get(i).getPiso().equals(this.getResources().getString(R.string.piso1))){
 
-                suma = suma + tam;
+                suma1 = suma1 + tam;
 
             }
-            promedio= suma/aux;
-            
+
+            if (apartamentos.get(i).getPiso().equals(this.getResources().getString(R.string.piso2))){
+
+                suma2 = suma2 + tam;
+
+            }
+
+            if (apartamentos.get(i).getPiso().equals(this.getResources().getString(R.string.piso3))){
+
+                suma3 = suma3 + tam;
+
+            }
+
+            if (apartamentos.get(i).getPiso().equals(this.getResources().getString(R.string.piso4))){
+
+                suma4 = suma4 + tam;
+
+            }
+
+            if (apartamentos.get(i).getPiso().equals(this.getResources().getString(R.string.piso5))){
+
+                suma5 = suma5 + tam;
+
+            }
 
         }
 
+        promedio1= suma1/aux;
+        resul1.setText("" + promedio1);
+        promedio2= suma2/aux;
+        resul2.setText("" + promedio2);
+        promedio3= suma3/aux;
+        resul3.setText("" + promedio3);
+        promedio4= suma4/aux;
+        resul4.setText("" + promedio4);
+        promedio5= suma5/aux;
+        resul3.setText("" + promedio5);
 
 
     }
